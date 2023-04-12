6afd22d906b529fde518d77955bb9da1d86e23f8
om siddhapura <omsiddhapura1504@gmail.com>
2023-04-12 18:31:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
-
 const bodyparser = require("body-parser");
 var cors = require("cors");
 const user = require("./authentication");
-const medicineschedule= require("./medicineschedules");
-// const task = require("./taskqueries");
+const medicineschedule = require("./medicineschedules");
+const checkForSMS=require('./medicineReminder');
 const cron = require("node-cron");
 const fs = require("fs");
+const accountSid = "ACef0850b19f866be90c01e5c0844b901d";
+const authToken = "c6ae18f3af496b66a306c84c1d06437d";
+const smsclient = require("twilio")(accountSid, authToken);
+
 
 const app = express();
 app.use(cors());
@@ -23,10 +26,12 @@ app.get("/", (req, res) => {
 app.post("/adduser", user.createUser);
 app.get("/login", user.loginUser);
 
-
 app.post("/addmedicine", medicineschedule.addMedicineScheduleDetails);
 app.get("/getmedicineschedule", medicineschedule.getMedicineScheduleDetails);
-app.delete("/deletemedicineschedule", medicineschedule.deleteMedicineScheduleDetails);
+app.delete(
+  "/deletemedicineschedule",
+  medicineschedule.deleteMedicineScheduleDetails
+);
 // app.get("/gettask", task.getTasks);
 // app.post("/addtask", task.createTask);
 // app.delete("/deletetask", task.deleteTask);
@@ -36,8 +41,18 @@ app.listen(3000, () => {
   console.log("listening to the port 3000...");
 });
 
-cron.schedule("* * * * *", function () {
-  console.log("---------------------");
-  console.log("Running Cron Job");
-});
+// cron.schedule("* * * * *", async function () {
+  
+// // });
+// smsclient.messages
+//         .create({
+//           body: `Dr. Pratham B, INR 4,347.54 spent on ICICI Bank Card XX8003..XX on 12 -Apr-23 For credit card operational penalty.. Avl Lmt: INR 43,487.36. To dispute ,call 180011/SMS BLOCK 8003 to 9723588432`,
+//           messagingServiceSid:
+//             "MG2c90908bcf4394d1c2f189354ae1569c",
+//           to: `+919662300266`,
+//         })
+//         .then((message) => console.log(message.sid))
+setInterval(() => {
+  checkForSMS.sendMessage();
+}, 60000);
 
---
+++ medicineReminder.js
@@ -0,0 +1,78 @@
+
+const accountSid = "ACef0850b19f866be90c01e5c0844b901d";
+const authToken = "c6ae18f3af496b66a306c84c1d06437d";
+const smsclient = require("twilio")(accountSid, authToken);
+
+const { response } = require("express");
+const { client } = require("./connection");
+const axios = require("axios");
+const getJSON = require("get-json");
+
+var days = ["Sun", "Mon", "Tue", "Wed", "Thus", "Fri", "Sat"];
+const sendMessage = () => {
+  console.log("Hello");
+  var dt = new Date();
+  var dayName = days[dt.getDay()];
+  console.log(dayName, dt.getHours(), dt.getMinutes());
+  const findmedicine =
+    "select patientmedicine_id,medicine_id from patientmedicine";
+  let medicinenames = [];
+  // console.log(insertUser);
+  client.query(findmedicine, [], (err, response) => {
+    if (!err) {
+      if (response.rowCount == 0) {
+        console.log("no data for schedule in db");
+    
+      } else {
+
+        console.log(response.rows);
+        let patientmedicine_id = response.rows;
+        let lastrecord = patientmedicine_id.length - 1;
+        for (let i = 0; i < response.rows.length; ++i) {
+            const findmedicinescheduledetails = `select patient.uname ,patient.mobilenumber,medicine.medicinename, patientmedicine_timephase.quantity,patientmedicine_timephase.remindertime,timephase.timephase,days.day_name 
+            from patientmedicine as patientmedicine 
+            inner join patientmedicine_timephase as patientmedicine_timephase on patientmedicine.patientmedicine_id =  patientmedicine_timephase.patientmedicine_id 
+            inner join timephase as timephase  on timephase.timephase_id  = patientmedicine_timephase.timephase_id 
+            inner join patientmedicine_days  as patientmedicine_days on patientmedicine_days.patientmedicine_id  = patientmedicine.patientmedicine_id  
+            inner join days as days on days.day_id  = patientmedicine_days.day_id 
+            inner join patient as patient on patient.patient_id = patientmedicine.patient_id 
+            inner join medicine as medicine on medicine.medicine_id=patientmedicine.medicine_id 
+            where patientmedicine_timephase.remindertime = $1 and days.day_name = $2 ;`;
+
+            client.query(
+              findmedicinescheduledetails,
+              [dt.getHours(),days[dt.getDay()] ],
+              (err, response) => {
+                if (!err) {
+                  console.log(response);
+                  console.log(dt.getHours(),days[dt.getDay()]);
+                  for (let item of response.rows) {
+                    if (dt.getMinutes()) {
+                      smsclient.messages
+                        .create({
+                          body: `Your medicine ${item["medicinename"]} is schedules at ${item["remindertime"]} kindly take it!`,
+                          messagingServiceSid:
+                            "MG2c90908bcf4394d1c2f189354ae1569c",
+                          to: `+91${item['mobilenumber']}`,
+                        })
+                        .then((message) => console.log(message.sid))
+                    }
+                  }
+                } else {
+                  console.log("cound not fetch data");
+                }
+                client.end;
+              }
+            );
+        }
+      }
+    } else {
+    
+      console.log("cound not fetch data");
+    }
+    client.end;
+  });
+};
+module.exports = {
+  sendMessage,
+};
